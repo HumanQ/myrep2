744ffb138cf1e87acf401623b3c4ce49a2430eb7
Man good <biem.net@gmail.com>
2013-07-06 17:05:12 +0000

    changes web to ok.js

--- web.js
+++ web.js
@@ -1,12 +1,7 @@
-var express = require('express');
-
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
   console.log(data);
-});
-
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
 });
