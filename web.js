8b33c1334d21b3eec83fafa41b92bd32a437df7c
HumanQ <biem.net@gmail.com>
2013-07-06 18:19:39 +0000

    change again and again and again

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
-fs = require('fs')
-fs.readFile('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+	var fs = require("fs");
+	var buffer = new buffer (100);
+  response.send(buffer.toString("utf-8", fs.readFileSync("index.html")));
 });
 
-
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
