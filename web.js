281c0aa7c26c2366144a5de3773d3d3c8ae9e57b
HumanQ <biem.net@gmail.com>
2013-07-06 21:32:01 +0000

    change in web.js ok ok ok ok ok not ok ok okokokok

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var fs = require('fs');
-	var buffer = new Buffer ('hello', 'utf-8');
-	console.log(buffer.toString);
-	console.log("hello");
+	var buffer = new Buffer (256);
+  response.send(buffer.toString('utf-8', fs.readFileSync("index.html")))
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
